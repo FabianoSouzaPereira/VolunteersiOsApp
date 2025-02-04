e7b986911e9737a923c763c0e53d85b38f4785bf
fabiano <jeovajire2003@gmail.com>
2025-02-04 20:32:59 -0300

    Initial Commit

---
+++ VolunteersiOsApp/VolunteersiOsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolunteersiOsAppApp.swift
+//  VolunteersiOsApp
+//
+//  Created by Fabiano Pereira on 04/02/25.
+//
+
+import SwiftUI
+
+@main
+struct VolunteersiOsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
